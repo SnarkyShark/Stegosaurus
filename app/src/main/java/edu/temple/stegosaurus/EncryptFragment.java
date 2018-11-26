eb37c1be21efbdd16b5361845712b3d0a87f1693
Will Johannesen <willjohannesen@gmail.com>
2018-11-26 14:48:29 -0500

    Revised insertion/extraction pages & made progress on sending image to server for insertion

--- app/src/main/java/edu/temple/stegosaurus/EncryptFragment.java
+++ app/src/main/java/edu/temple/stegosaurus/EncryptFragment.java
@@ -1,24 +1,32 @@
 package edu.temple.stegosaurus;
 
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,11 +34,16 @@ import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -38,20 +51,24 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static android.app.Activity.RESULT_OK;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EncryptFragment extends Fragment {
 
-    Button button, testButton;
+    Button basePhotoButton, dataPhotoButton, insertButton;
     TextView t;
-    String message, key;
-    EditText msg_box, key_box;
-    boolean changed = false;
+    ImageView baseImageView, dataImageView;
+    Uri baseImageUri, dataImageUri;
     View v;
 
-    private static final int PICK_IMAGE = 123456;
+    String testMessage = "test test";
+
+    private static final int PICK_BASE_IMAGE = 100;
+    private static final int PICK_DATA_IMAGE = 101;
 
     public EncryptFragment() {
         // Required empty public constructor
@@ -63,75 +80,179 @@ public class EncryptFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_encrypt, container, false);
-
-        message = "";
-        key = "";
         t = v.findViewById(R.id.encryptTime);
-        msg_box = v.findViewById(R.id.msgEditText);
-        key_box = v.findViewById(R.id.keyEditText);
+        baseImageView = v.findViewById(R.id.basePhotoView);
+        dataImageView = v.findViewById(R.id.dataPhotoView);
+        basePhotoButton = v.findViewById(R.id.basePhotoButton);
+        dataPhotoButton = v.findViewById(R.id.dataPhotoButton);
+        insertButton = v.findViewById(R.id.insertButton);
 
-        button = v.findViewById(R.id.encryptButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        basePhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                testRetro();
+                openGallery(PICK_BASE_IMAGE);
             }
         });
 
-        testButton = v.findViewById(R.id.testButton);
-        testButton.setOnClickListener(new View.OnClickListener() {
+        dataPhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                testInputs();
+                openGallery(PICK_DATA_IMAGE);
+            }
+        });
+
+        insertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getCapacity();
             }
         });
 
         return v;
     }
 
-    public void testInputs() {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == PICK_BASE_IMAGE) {
+            baseImageUri = data.getData();
+            //send it to baseImageHandler
+            Message msg = Message.obtain();
+            msg.obj = PICK_BASE_IMAGE;
+            imageHandler.sendMessage(msg);
+        }
+        if(resultCode == RESULT_OK && requestCode == PICK_DATA_IMAGE) {
+            dataImageUri = data.getData();
+            //send it to baseImageHandler
+            Message msg = Message.obtain();
+            msg.obj = PICK_DATA_IMAGE;
+            imageHandler.sendMessage(msg);
+        }
+    }
+
+    /**
+     *  Stegosaurus API functions
+     */
+
+    public void getCapacity() {
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl("https://stegosaurus.ml")
                 .addConverterFactory(GsonConverterFactory.create());
-
         Retrofit retrofit = builder.build();
-
         StegosaurusService client = retrofit.create(StegosaurusService.class);
+        if(baseImageUri != null) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PICK_BASE_IMAGE);
+            if (ContextCompat.checkSelfPermission(v.getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Call<String> call = client.howManyBytes(prepareFilePart("image", baseImageUri), false);
+                call.enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        Message msg = Message.obtain();
+                        msg.obj = response.body();
+                        textViewHandler.sendMessage(msg);
+                    }
 
-        //get images
-        //create call --> method
-        //enqueue call
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+                        Toast.makeText(getActivity(), "Can't get capacity", Toast.LENGTH_SHORT).show();
+                        Log.i("Throwable", t.toString());
+                    }
+                });
+            }
+            else
+                Toast.makeText(getActivity(), "external storage: " + isExternalStorageWritable(), Toast.LENGTH_SHORT).show();
+
+        }
     }
 
-    public void testRetro() {
+    public void getStegoImage() {
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl("https://stegosaurus.ml")
                 .addConverterFactory(GsonConverterFactory.create());
-
         Retrofit retrofit = builder.build();
-
         StegosaurusService client = retrofit.create(StegosaurusService.class);
-        Call<String> call = client.basicResponse();
+        if(baseImageUri != null && dataImageUri != null) {  // TODO: allow text file or photo THEN allow photo or other small file
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PICK_BASE_IMAGE);
+            if (ContextCompat.checkSelfPermission(v.getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Call<String> call = client.howManyBytes(prepareFilePart("image", baseImageUri), false);
+                call.enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        Message msg = Message.obtain();
+                        msg.obj = response.body();
+                        textViewHandler.sendMessage(msg);
+                    }
 
-        call.enqueue(new Callback<String>() {
-            @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                Message msg = Message.obtain();
-                msg.obj = response.body();
-                networkHandler.sendMessage(msg);
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+                        Toast.makeText(getActivity(), "Can't get capacity", Toast.LENGTH_SHORT).show();
+                        Log.i("Throwable", t.toString());
+                    }
+                });
             }
+            else
+                Toast.makeText(getActivity(), "external storage: " + isExternalStorageWritable(), Toast.LENGTH_SHORT).show();
 
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                Toast.makeText(getActivity(), "something went wrong", Toast.LENGTH_SHORT).show();
-            }
-        });
+        }
+        else
+            Toast.makeText(getActivity(), "Please provide all inputs", Toast.LENGTH_SHORT).show();
     }
 
-    Handler networkHandler = new Handler(new Handler.Callback() {
+    /**
+     *  Helper functions
+     */
+
+    private MultipartBody.Part prepareFilePart(String partName, Uri fileUri) {
+        File file = FileUtils.getFile(getActivity(), fileUri);
+
+        // create RequestBody instance from file
+        RequestBody requestFile =
+                RequestBody.create(
+                        MediaType.parse(getActivity().getContentResolver().getType(fileUri)),
+                        file
+                );
+
+        // MultipartBody.Part is used to send also the actual file name
+        return MultipartBody.Part.createFormData(partName, file.getName(), requestFile);
+    }
+
+    public void openGallery(int resultCode) {
+        Intent gallery = new Intent (Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, resultCode);
+    }
+
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *  Handlers
+     */
+
+    Handler textViewHandler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(Message msg) {
             t.setText((String) msg.obj);
+            return false;
+        }
+    });
+
+    Handler imageHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            int imageRequestCode = (int) msg.obj;
+            if (imageRequestCode == PICK_BASE_IMAGE)
+                baseImageView.setImageURI(baseImageUri);
+            if (imageRequestCode == PICK_DATA_IMAGE)
+                dataImageView.setImageURI(dataImageUri);
             return false;
         }
     });
--- app/src/main/java/edu/temple/stegosaurus/StegosaurusService.java
+++ app/src/main/java/edu/temple/stegosaurus/StegosaurusService.java
@@ -12,13 +12,21 @@ import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface StegosaurusService {
 
     @GET("/api/test")
     Call<String> basicResponse();
 
+    @POST("/api/test")
+    Call<String> echoResponse(@Query("message") String message);
+
+    @Multipart
+    @POST("/api/get_capacity")
+    Call<String> howManyBytes(@Part MultipartBody.Part theImage, @Query("formatted") boolean formatted);
+
     @Multipart
     @POST("/api/insert")
-    Call<String> insertPhoto(@Part MultipartBody.Part baseImage, @Part MultipartBody.Part dataImage, @Part("KEY") RequestBody key);
+    Call<String> insertPhoto(@Part MultipartBody.Part baseImage, @Part MultipartBody.Part dataImage, @Query("key") String key);
 }
